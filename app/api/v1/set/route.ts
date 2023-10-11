68efdb5601ea243ef95003125d8393ac7536f831
DenisKurek <denis4kurek@gmail.com>
2023-10-11 22:11:29 +0200

    adding mongoDB integration

---
+++ app/api/v1/set/route.ts
@@ -0,0 +1,33 @@
+import Set from "@/lib/model/Set";
+import { conntectToDatabase } from "@/lib/utils/db";
+import { NextResponse } from "next/server";
+export async function POST(request: Request) {
+  const set: Set = await request.json();
+  try {
+    const client = await conntectToDatabase();
+    const db = client.db();
+    const collection = db.collection("sets");
+    const result = await collection.insertOne(set);
+    return NextResponse.json({ result });
+  } catch (error) {
+    console.log("ERROR");
+    return new Response("ERROR", {
+      status: 500,
+    });
+  }
+}
+
+export async function GET(request: Request) {
+  try {
+    const client = await conntectToDatabase();
+    const db = client.db();
+    const collection = db.collection("sets");
+    const sets = await collection.find({}).toArray();
+    return NextResponse.json({ sets });
+  } catch (error) {
+    console.log("ERROR");
+    return new Response("ERROR", {
+      status: 500,
+    });
+  }
+}
--- components/SetsList.tsx
+++ components/SetsList.tsx
@@ -1,28 +1,30 @@
-const sets = [
-  {
-    title: "set1",
-  },
-  {
-    title: "set1",
-  },
-  {
-    title: "set1",
-  },
-  {
-    title: "set1",
-  },
-  {
-    title: "set1",
-  },
-];
+"use client";
+import { useEffect, useState } from "react";
+import Set from "@/lib/model/Set";
 
 const SetsList = () => {
+  const [sets, setSets] = useState<Set[]>([]);
+  useEffect(() => {
+    async function getSets() {
+      const response = await fetch("api/v1/set", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const { sets } = await response.json();
+
+      console.log(sets);
+      setSets(sets);
+    }
+    getSets();
+  }, []);
   return (
     <ul className=" list-group d-flex justify-content-evenly">
       <div className="row">
         {sets.map((set, index) => (
           <li className=" list-group-item m-2" key={index}>
-            {set.title}
+            {set.name}
           </li>
         ))}
       </div>
