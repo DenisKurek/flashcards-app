80c45acb43f62c95734795f13254425c7791cc72
deniskurek <denis4kurek@gmail.com>
2023-10-14 23:34:18 +0200

    extrating set form to separate component

--- app/new-set/page.tsx
+++ app/new-set/page.tsx
@@ -1,61 +1,26 @@
 "use client";
-import FlashcardForm from "@/components/new-set/FlashcardForm";
-import NewFlashCard from "@/components/new-set/NewFlashCard";
 import {SetBlueprint} from "@/lib/model/Set";
-import Flashcard from "@/lib/model/FlashCard";
-import { useRef, useState } from "react";
+import SetEditionForm from "@/components/edit-set/SetEditionForm";
+import { useRouter } from "next/navigation";
 
-async function createSet(set: SetBlueprint) {
-  const response = await fetch("api/v1/set", {
-    method: "POST",
-    body: JSON.stringify(set),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-}
 
 export default function HomePage() {
-  const [flashCards, setFlashcards] = useState<Flashcard[]>([]);
-  const nameRef:any = useRef();
+  const router = useRouter();
 
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    createSet({
-      name: nameRef.current.value,
-      flashcards: flashCards,
+  const createSet = async (set: SetBlueprint) => {
+    const response = await fetch("api/v1/set", {
+      method: "POST",
+      body: JSON.stringify(set),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-  };
-
-  const handleFlashCardAddition = (flashCard: Flashcard) => {
-    setFlashcards((prev) => prev.concat(flashCard));
-  };
-
-  const handleFlashCardUpdate = (updatedFlashCard: Flashcard) => {
-    setFlashcards((prev) => prev.map((flashCard)=> flashCard.id === updatedFlashCard.id? updatedFlashCard: flashCard ));
-  };
-
-  const handleFlashCardDelete = (deleteId: number) => {
-    setFlashcards((prev) => prev.filter((flashCard)=> flashCard.id!= deleteId));
-  };
+    if(response.ok){
+      router.push("/")
+    }
+  }
 
   return (
-    <form className="container bg-dark" onSubmit={handleSubmit}>
-      <label htmlFor="set-name">Set Name: </label>
-      <input id="set-name" type="text" className=" form-control" defaultValue={"new Set"} ref={nameRef} required/>
-      <ul className=" list-group ">
-        {flashCards.map((flashCard) => (
-          <FlashcardForm 
-          flashCard={flashCard} 
-          key={flashCard.id}
-          onUpdate={handleFlashCardUpdate}
-          onDelete={handleFlashCardDelete}/>
-        ))}
-        <NewFlashCard onClick={handleFlashCardAddition} />
-      </ul>
-      <button className="btn-primary" type="submit">
-        Create
-      </button>
-    </form>
+    <SetEditionForm set={{name: "newSet", flashcards: []}} onSubmit={createSet}/>
   );
 }
--- lib/model/Set.ts
+++ lib/model/Set.ts
@@ -3,8 +3,6 @@ import Flashcard from "./FlashCard";
 
 export default interface Set extends SetBlueprint{
   _id: ObjectId;
-  name: string;
-  flashcards: Flashcard[];
 }
 
 export interface SetBlueprint {
