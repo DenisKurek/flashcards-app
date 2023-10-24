0cae1f051cddafa8f7438a7b7767dcdbd869aaaa
DenisKurek <denis4kurek@gmail.com>
2023-10-24 21:01:51 +0200

    adding integration with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  daisyui: {
+    themes: ["cupcake", "dark", "cmyk"],
+  },
+  plugins: [require("daisyui")],
+};
