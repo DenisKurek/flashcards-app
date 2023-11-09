8db22d81d2a047c2f93034de2ee7669e5d64f23a
DenisKurek <denis4kurek@gmail.com>
2023-11-09 12:13:56 +0100

    Replacing delete button with delete Icon

--- components/edit-set/FlashcardForm.tsx
+++ components/edit-set/FlashcardForm.tsx
@@ -1,5 +1,6 @@
 import Flashcard, { FlashcardBlueprint } from "@/lib/model/FlashCard";
 import React from "react";
+import CloseIcon from "../ui/CloseIcon";
 
 interface Props {
   flashCard: Flashcard;
@@ -20,45 +21,48 @@ const FlashcardForm: React.FC<Props> = (props) => {
   };
 
   return (
-    <li
-      key={props.flashCard.id}
-      className="card m-2 flex space-y-2 bg-gray-600 p-4"
-    >
-      <label htmlFor={`concept-${props.flashCard.id}`}>Concept:</label>
-      <input
-        type="text"
-        className="input input-bordered input-secondary w-full "
-        id={`concept-${props.flashCard.id}`}
-        defaultValue={props.flashCard.concept}
-        placeholder="concept"
-        onChange={(e) =>
-          handleUpdate({
-            concept: e.target.value,
-            definition: props.flashCard.definition,
-          })
-        }
-      />
-      <label htmlFor={`definition-${props.flashCard.id}`}>Definition:</label>
-      <input
-        type="text"
-        className="input input-bordered input-secondary w-full "
-        id={`definition-${props.flashCard.id}`}
-        defaultValue={props.flashCard.definition}
-        placeholder="definition"
-        onChange={(e) =>
-          handleUpdate({
-            concept: props.flashCard.concept,
-            definition: e.target.value,
-          })
-        }
-      />
-      <button
-        key={`delete-${props.flashCard.id}`}
-        className="btn bg-primary"
-        onClick={handleDelate}
-      >
-        delete
-      </button>
+    <li key={props.flashCard.id} className="card m-2 bg-gray-600">
+      <div className="card-body p-1">
+        <div className="card-actions justify-end">
+          <button className="btn-sm">
+            <CloseIcon onRemove={handleDelate} />
+          </button>
+        </div>
+        <div className="flex p-2">
+          <label className="label" htmlFor={`concept-${props.flashCard.id}`}>
+            Concept:
+          </label>
+          <input
+            type="text"
+            className="input input-bordered input-secondary "
+            id={`concept-${props.flashCard.id}`}
+            defaultValue={props.flashCard.concept}
+            placeholder="concept"
+            onChange={(e) =>
+              handleUpdate({
+                concept: e.target.value,
+                definition: props.flashCard.definition,
+              })
+            }
+          />
+          <label className="label" htmlFor={`definition-${props.flashCard.id}`}>
+            Definition:
+          </label>
+          <input
+            type="text"
+            className="input input-bordered input-secondary "
+            id={`definition-${props.flashCard.id}`}
+            defaultValue={props.flashCard.definition}
+            placeholder="definition"
+            onChange={(e) =>
+              handleUpdate({
+                concept: props.flashCard.concept,
+                definition: e.target.value,
+              })
+            }
+          />
+        </div>
+      </div>
     </li>
   );
 };
---
+++ components/ui/CloseIcon.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  onRemove: () => void;
+}
+
+const CloseIcon: React.FC<Props> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      className="inline-block h-4 w-4 cursor-pointer stroke-current"
+      onClick={props.onRemove}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M6 18L18 6M6 6l12 12"
+      ></path>
+    </svg>
+  );
+};
+
+export default CloseIcon;
