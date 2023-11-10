114887b11089e9e6a936f9ed530da3965d8087f3
DenisKurek <denis4kurek@gmail.com>
2023-11-10 13:30:02 +0100

    Adding optional Components To tags

--- components/edit-set/set-details/TagSection.tsx
+++ components/edit-set/set-details/TagSection.tsx
@@ -2,16 +2,27 @@ import CloseIcon from "@/components/ui/CloseIcon";
 
 interface Props {
   tags: string[];
-  onRemove: (tag: string) => void;
+  onRemove?: (tag: string) => void;
+  onClick?: (tag: string) => void;
 }
 
 const TagSection: React.FC<Props> = (props) => {
   return (
     <div className="container p-2">
       {props.tags.map((tag) => (
-        <div key={tag} className="badge badge-secondary m-2 gap-2">
+        <div
+          key={tag}
+          className={`badge badge-secondary m-2 gap-2 ${
+            props.onClick ? "cursor-pointer " : ""
+          }`}
+          onClick={() => props.onClick && props.onClick(tag)}
+        >
           {tag}
-          <CloseIcon onRemove={() => props.onRemove(tag)} />
+          {props.onRemove ? (
+            <CloseIcon onRemove={() => props.onRemove && props.onRemove(tag)} />
+          ) : (
+            ""
+          )}
         </div>
       ))}
     </div>
