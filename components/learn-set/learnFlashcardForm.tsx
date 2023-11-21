f2a42e8e2d2320bebc0bf488415b4b154e960417
DenisKurek <denis4kurek@gmail.com>
2023-11-21 22:02:32 +0100

    flashcard State Update

--- components/learn-set/learnFlashcardForm.tsx
+++ components/learn-set/learnFlashcardForm.tsx
@@ -1,16 +1,17 @@
 import Flashcard from "@/lib/model/FlashCard";
 import { useRouter } from "next/navigation";
-import { SyntheticEvent, useState } from "react";
+import { SyntheticEvent, useState, useRef } from "react";
 import EditIcon from "../ui/EditIcon";
 import FlipIcon from "../ui/FlipIcon";
 
 interface Props<> {
-  onSubmit: (e: SyntheticEvent) => void;
+  onSubmit: (e: SyntheticEvent, actual: string, expected: string) => void;
   flashCard: Flashcard;
   setId: string;
 }
 
 const LearnFlashcardForm: React.FC<Props> = (props) => {
+  const answerRef = useRef<any>();
   const router = useRouter();
   const [fliped, setFliped] = useState<boolean>(true);
   const [placecholder, setPlacecholder] = useState<string>();
@@ -23,11 +24,21 @@ const LearnFlashcardForm: React.FC<Props> = (props) => {
     return !fliped ? props.flashCard.concept : props.flashCard.definition;
   };
 
+  const handleFlip = () => {
+    setPlacecholder(undefined);
+    setFliped((prev) => !prev);
+  };
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    props.onSubmit(e, answerRef.current.value, getHiddenPart(fliped));
+    answerRef.current.value = "";
+  };
+
   return (
-    <form className="card bg-neutral" onSubmit={props.onSubmit}>
+    <form className="card bg-neutral" onSubmit={handleSubmit}>
       <div className="card-body">
         <div className="card-actions justify-end">
-          <FlipIcon onClick={() => setFliped((prev) => !prev)} />
+          <FlipIcon onClick={handleFlip} />
           <EditIcon onClick={() => router.push(`/edit-set/${props.setId}`)} />
         </div>
         <h2 className="card-title justify-center">
@@ -41,7 +52,8 @@ const LearnFlashcardForm: React.FC<Props> = (props) => {
           id="answer"
           type="text"
           className="input input-bordered input-primary"
-          placeholder={placecholder && placecholder}
+          ref={answerRef}
+          placeholder={placecholder}
         />
 
         <div className="card-actions justify-end">
